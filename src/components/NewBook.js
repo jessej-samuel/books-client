f37632673d98157a28c23215cf30cf0273479d0b
jessej-samuel <1234jessej@gmail.com>
2022-12-26 13:56:50 +0530

    add: title to new book form

--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -27,34 +27,37 @@ const NewBook = () => {
   };
 
   return (
-    <form
-      onSubmit={onSubmit}
-      className="bg-black/10 p-1 grid grid-cols-4 grid-flow-row gap-1"
-    >
-      <label htmlFor="title">Title</label>
-      <input
-        id="title"
-        type={"text"}
-        value={data.title}
-        className="col-span-3"
-        onChange={(e) => setData({ ...data, title: e.target.value })}
-      />
+    <>
+      <h2 className="font-medium text-xl">Add a new book?</h2>
+      <form
+        onSubmit={onSubmit}
+        className="bg-black/10 p-1 grid grid-cols-4 grid-flow-row gap-1"
+      >
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          type={"text"}
+          value={data.title}
+          className="col-span-3"
+          onChange={(e) => setData({ ...data, title: e.target.value })}
+        />
 
-      <label htmlFor="author">Author</label>
-      <input
-        id="author"
-        type={"text"}
-        value={data.author}
-        className="col-span-3"
-        onChange={(e) => setData({ ...data, author: e.target.value })}
-      />
+        <label htmlFor="author">Author</label>
+        <input
+          id="author"
+          type={"text"}
+          value={data.author}
+          className="col-span-3"
+          onChange={(e) => setData({ ...data, author: e.target.value })}
+        />
 
-      <input
-        type={"submit"}
-        value="Create new book"
-        className="bg-green-400 text-white py-1 px-2 rounded col-span-full"
-      />
-    </form>
+        <input
+          type={"submit"}
+          value="Create new book"
+          className="bg-green-400 text-white py-1 px-2 rounded col-span-full"
+        />
+      </form>
+    </>
   );
 };
 
